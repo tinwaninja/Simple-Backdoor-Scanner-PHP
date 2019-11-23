221c9751b562246f66ac748ab9c39cab8ee45992
Tn. Ninja <tinwaninja@gmail.com>
2019-11-23 11:47:52 +0700

    Update scanner-cli.php

--- scanner-cli.php
+++ scanner-cli.php
@@ -1,5 +1,6 @@
 <?php
 set_time_limit(0);
+ini_set("memory_limit", -1);
 error_reporting(0);
 @ini_set('zlib.output_compression', 0);
 header("Content-Encoding: none");
@@ -18,55 +19,41 @@ function ngelist($dir, &$keluaran = array()) {
     return $keluaran;
 }
 function baca($filenya) {
-	$filesize = filesize($filenya);
-	$filesize = round($filesize / 1024 / 1024, 1);
-	if($filesize>2) { //max 2mb
-		$kata = "Skipped--";
-		echo $kata;
-		$fp = fopen('result-scanner.txt', 'a');
-		fwrite($fp, $kata);
-		fclose($fp);
-	}else {
-		$php_file = file_get_contents($filenya);
-		$tokens   = token_get_all($php_file);
-		$keluaran = array();
-		$batas    = count($tokens);
-		if ($batas > 0) {
-			for ($i = 0; $i < $batas; $i++) {
-				if (isset($tokens[$i][1])) {
-					$keluaran[] .= $tokens[$i][1];
-				}
-			}
-		}
-		$keluaran = array_values(array_unique(array_filter(array_map('trim', $keluaran))));
-		return ($keluaran);
-	}
+        $filesize = filesize($filenya);
+        $filesize = round($filesize / 1024 / 1024, 1);
+        if($filesize>2) { //max 2mb
+                $kata = "Skipped--";
+                echo $kata;
+                /*$fp = fopen('result-scanner.txt', 'a');
+                fwrite($fp, $kata);
+                fclose($fp);*/
+        }else {
+                $php_file = file_get_contents($filenya);
+                $tokens   = token_get_all($php_file);
+                $keluaran = array();
+                $batas    = count($tokens);
+                if ($batas > 0) {
+                        for ($i = 0; $i < $batas; $i++) {
+                                if (isset($tokens[$i][1])) {
+                                        $keluaran[] .= $tokens[$i][1];
+                                }
+                        }
+                }
+                $keluaran = array_values(array_unique(array_filter(array_map('trim', $keluaran))));
+                return ($keluaran);
+        }
 }
 function ngecek($string) {
     //tambahkan nama fungsi, class, variable yang sering digunakan pada backdoor
     //add name of the function, class, variable that is often used on the backdoor
     $dicari   = array(
-        'base64_encode',
         'base64_decode',
-        'FATHURFREAKZ',
         'eval',
         'gzinflate',
         'str_rot13',
         'convert_uu',
         'shell_data',
-        'getimagesize',
-        'magicboom',
-        'exec',
-        'shell_exec',
-        'fwrite',
-        'str_replace',
-        'mail',
-        'file_get_contents',
-        'url_get_contents',
-        'symlink',
-        'substr',
-        '__file__',
-        '__halt_compiler'
+        'symlink'
     );
     $keluaran = "";
     foreach ($dicari as $value) {
@@ -88,17 +75,17 @@ foreach ($list as $value) {
         $cek    = ngecek($string);
         if (empty($cek)) {
             $kata = $value ." => Safe\n";
-			echo $kata;
-			$fp = fopen('result-scanner.txt', 'a');
-			fwrite($fp, $kata);
-			fclose($fp);
-        } else {
+                        echo $kata;
+        } else if(preg_match("/, /", $cek)) {
             $kata = $value ." => Found (". $cek .")\n";
-			echo $kata;
-			$fp = fopen('result-scanner.txt', 'a');
-			fwrite($fp, $kata);
-			fclose($fp);
-        }
+                        echo $kata;
+                        $fp = fopen('result-scanner.txt', 'a');
+                        fwrite($fp, $kata);
+                        fclose($fp);
+        }else{
+			$kata = $value ." => Found (". $cek .")\n";
+                        echo $kata;
+		}
         ob_flush();
         flush();
         sleep(1);
